3a12cdd9895cfa992d945a6535d85238013600ed
LauraMcCormack <laura.mccormack227@gmail.com>
2012-05-31 15:27:51 +0100

    stores urls with ids, redirect does not work

--- linkShortener.rb
+++ linkShortener.rb
@@ -10,21 +10,31 @@ get '/' do
 end
 
 post '/' do
-	big_url = request.body.url
+	@big_url = params["url"]
 	small_id = urls.length+1
-
-	urls.store(small_id,big_url)
-	urls.store(big_url,small_id)
-	@small_url = base+'/'+small_id
-	haml :urlstored
+	if urls.has_key?(@big_url)
+		redirect to('/')
+	else
+		urls.store(small_id,@big_url)
+		urls.store(@big_url,small_id)
+		@small_url = "#{base}#{small_id}"
+		haml :urlstored
+	end
 	
 end
 
-get '/url/:small' do 
+get '/:small' do 
 	if urls[params[:small]] == nil 
-		redirect to('/')
+ 		urls.each do |one,two|
+			puts "#{one}: #{two}"		
+		end
+		big_url = urls[params[:small]]
+		puts big_url
+		puts urls.length
+		#redirect to('/')
 	else
 		big_url = urls[params[:small]]
+		puts big_url
 		redirect to(big_url)
 	end
 end
