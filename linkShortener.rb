8a3f2580aa76c10853256f57a87b1a989e8de704
LauraMcCormack <laura.mccormack227@gmail.com>
2012-06-01 16:18:16 +0100

    link with mongodb, in comments not done yet #5

--- linkShortener.rb
+++ linkShortener.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 require 'haml'
+require 'mongo'
 
 set :haml, :format => :html5
 base = "http://localhost:4567/"
 urls=Hash.new
+conn = Mongo::Connection.new #(host, port)
+db   = conn['links']
+coll = db['link']
 
 get '/' do
 	haml :index
@@ -13,14 +17,15 @@ post '/' do
 	@big_url = params["url"]
 	small_id = urls.length+1
 	if urls.has_key?(@big_url)
-		puts "#{@big_url}"
 		@small = urls["#{@big_url}"]
-		puts @small
+		#@small = 
 		haml :existsAlready
 	#page which shows big url and small one already stored
 	else
 		urls.store(small_id,@big_url)
 		urls.store(@big_url,small_id)
+		#coll.insert({small_id => @big_url})
+		#coll.insert({@big_url => small_id})
 		@small_url = "#{base}#{small_id}"
 		haml :urlstored
 	end
@@ -28,8 +33,9 @@ post '/' do
 end
 
 get '/:small' do |id|
-	if urls.has_key?("#{id}".to_i)
+	if urls.has_key?("#{id}".to_i) #if
 		big_url = urls["#{id}".to_i]
+		#big_url =
 		redirect to("http://#{big_url}")
 	else
 		haml :error
